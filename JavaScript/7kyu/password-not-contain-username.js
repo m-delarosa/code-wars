bd7726ef41c3d6338d755e79ddf3c9f831fd9e25
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-14 20:44:16 -0600

    Update password.js

--- JavaScript/7kyu/password-not-contain-username.js
+++ JavaScript/7kyu/password-not-contain-username.js
@@ -12,3 +12,54 @@
 
 // Both the username and the password may contain uppercase letters, lowercase letters, numbers, spaces, and the following special/punctation characters: !@#$%^&*()_+{}:"<>?[];',.
 // The username and password will each be less than 100 characters.
+
+/*
+Pseudo
+Identify the shortest string of the two and assign short and long variables to each
+    password.length > username.length
+        ? const short = username
+        : const short
+
+*/
+
+const username = "MASH"
+const password = "M*A*S*H"
+const finalU = "aa*aa"
+const finalP = "aaaaaa"
+
+function validate(username, password) {
+    let short, long
+
+    if (username.length >= password.length) {
+        short = password
+        long = username
+    } else {
+        short = username
+        long = password
+    }
+
+    console.log("short", short)
+    console.log("long", long)
+
+
+    const limit = Math.ceil(short.length / 2)
+    console.log("limit", limit)
+
+    if (short.length === 1) {
+        return long.includes(short)
+            ? false
+            : true
+    }
+
+    for (let i = 0; i <= short.length - limit; i++) {
+        console.log("substring", short.substring(i, i + limit))
+        console.log("loop condition", long.includes(short.substring(i, i + limit)))
+
+        if (long.includes(short.substring(i, i + (limit - 1)))) return false
+    }
+
+    return true
+}
+
+// console.log("Returns", validate(username, password))
+console.log("Returns", validate(finalU, finalP))
