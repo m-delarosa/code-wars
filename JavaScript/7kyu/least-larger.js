1ebaa41a4ca98456cefb17dd9028738353605caf
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-15 20:28:33 -0600

    Finalize least-larger.js

--- JavaScript/7kyu/least-larger.js
+++ JavaScript/7kyu/least-larger.js
@@ -12,12 +12,26 @@
 // leastLarger( [4, 1, 3, 5, 6], 4 )  => -1
 
 const a = [4, 1, 3, 5, 6]
-const i = 3
+const i = 4
 
+//Performance w/o if statement on line 22: 874
+//Performance w/ line 22: 937
 function leastLarger(a, i) {
-
-
-    return -1
+    const targetVal = a[i]
+    const largerVals = a.filter(num => num > targetVal)
+    // if (largerVals.length === 0) return -1
+    const leastLargeVal = Math.min(...largerVals)
+    return a.findIndex(num => num === leastLargeVal)
 }
 
 console.log("Returns", leastLarger(a, i))
+
+/*
+Learns:
+
+The findIndex() method returns the index of the first element in the array that satisfies the provided testing function. Otherwise, it returns -1, indicating that no element passed the test.
+
+The most common way to find the min or max of an array is to pass in the array using the spread operator into Math.min or max.
+Though for large data sets, the most performant would be a for loop or Math.min.apply
+ref: https://medium.com/coding-at-dawn/the-fastest-way-to-find-minimum-and-maximum-values-in-an-array-in-javascript-2511115f8621
+*/
