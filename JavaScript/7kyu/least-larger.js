84631ec7517f9a02b35dda1d4727b12f8e111c87
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-15 20:09:36 -0600

    Add least-larger.js

---
+++ JavaScript/7kyu/least-larger.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/5f8341f6d030dc002a69d7e4/train/javascript
+
+// Given an array of numbers and an index, return the index of the least number larger than the element at the given index, or -1 if there is no such index ( or, where applicable, Nothing or a similarly empty value ).
+
+// Notes
+// Multiple correct answers may be possible. In this case, return any one of them.
+// The given index will be inside the given array.
+// The given array will, therefore, never be empty.
+
+// Example
+// leastLarger( [4, 1, 3, 5, 6], 0 )  =>  3
+// leastLarger( [4, 1, 3, 5, 6], 4 )  => -1
+
+const a = [4, 1, 3, 5, 6]
+const i = 3
+
+function leastLarger(a, i) {
+
+
+    return -1
+}
+
+console.log("Returns", leastLarger(a, i))
