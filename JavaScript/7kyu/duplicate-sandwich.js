190e425945749f6c8ac26015c89657c5975b99a8
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-18 20:24:38 -0600

    Modify duplicate-sandwich.js

--- JavaScript/7kyu/duplicate-sandwich.js
+++ JavaScript/7kyu/duplicate-sandwich.js
@@ -17,8 +17,11 @@
 /* Psuedo
 Create a hash map to store values
 Find the duplicate in the array
-Find the position of the duplicates within the array
-Return a subarray containing the elements in between
+Find the position of the first duplicate within the array
+Push a subarray of everything after the dup into the result array
+Find the index of the 2nd dup in the result array
+Slice away from that index on in the subarray
+Return the result array
 */
 
 const a = [0, 1, 2, 3, 4, 5, 6, 1, 7, 8]
@@ -26,29 +29,49 @@ const a = [0, 1, 2, 3, 4, 5, 6, 1, 7, 8]
 function duplicateSandwich(a) {
     let valueMap = {}
     let result = []
-    let duplicate
+    let duplicate, inputArr
 
-    a.forEach(el => {
+    typeof a === "string"
+        ? inputArr = a.split("")
+        : inputArr = a
+
+    inputArr.forEach(el => {
         valueMap[el]
             ? valueMap[el]++
             : valueMap[el] = 1
     })
+    console.log("valueMap", valueMap)
 
     for (const prop in valueMap) {
         if (valueMap[prop] === 2) duplicate = prop
     }
+    // console.log("duplicate", duplicate)
 
-    console.log(typeof a[0] === Number)
-    if (typeof a[0] === Number) duplicate = parseInt(duplicate)
+    if (typeof a[0] === "number") duplicate = parseInt(duplicate)
+    if (typeof a[0] === "boolean") duplicate = (duplicate === "true")
+    console.log("Type", typeof a[0])
 
     console.log("Duplicate is", duplicate, "as a", typeof duplicate)
 
-    const firstDup = a.findIndex(el => el === duplicateSandwich)
-    console.log(firstDup)
+    const firstDupIndex = inputArr.findIndex(el => el === duplicate)
+    console.log("first duplicate is located at", firstDupIndex)
 
 
+
+    result = inputArr.slice(firstDupIndex + 1)
+
+    const secondDupIndex = result.findIndex(el => el === duplicate)
+    console.log(secondDupIndex)
+
+    result.splice(secondDupIndex)
+
+    if (typeof a === "string") result = result.join("")
 
     return result
 }
 
 console.log("Returns", duplicateSandwich(a))
+
+/* Learns
+In order to use typeof in a conditional statement, you need to compare to a string as the result of typeof. See above.
+*/
