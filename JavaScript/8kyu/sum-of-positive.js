e4c548b04e7ab115840a020b6b365f53b13f0a4a
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-21 23:41:13 -0600

    Modify sum-of-positive.js

--- JavaScript/8kyu/sum-of-positive.js
+++ JavaScript/8kyu/sum-of-positive.js
@@ -37,6 +37,11 @@ const positiveSum = arr => {
     }, 0)
 }
 
+// Found Solution I admire
+function positiveSum(arr) {
+    return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0)
+}
+
 console.log(positiveSum([1, 2, 3, 4, 5]), 15)
 console.log(positiveSum([1, -2, 3, 4, 5]), 13)
 console.log(positiveSum([]), 0)
