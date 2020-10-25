8a94d89303346e8a080e52c025bcac000d7755e8
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-24 23:18:16 -0600

    Add opposite-number.js

---
+++ JavaScript/8kyu/opposite-number.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+
+// Very simple, given a number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+/* Pseudo
+If number is positive return negative number
+If number is negative return positive number
+*/
+
+function opposite(number) {
+    //your code here
+}
