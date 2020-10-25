3a649f3cf114168cec9211a71bb92f591915911e
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-24 23:41:26 -0600

    Add highest-and-lowest.js

---
+++ JavaScript/7kyu/highest-and-lowest.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/554b4ac871d6813a03000035
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number (in a string, seperated by a space).
+
+// Example:
+
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes:
+
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+/* Pseudo Code
+Split the string into an array, sort the array.
+return a template literal string with referencing the first and last index of the array in correct places.
+*/
