0c2e7573726f920b9b26b50b5a31871dd5e0ffcd
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-24 23:57:38 -0600

    Finalize highest-and-lowest.js

--- JavaScript/7kyu/highest-and-lowest.js
+++ JavaScript/7kyu/highest-and-lowest.js
@@ -17,3 +17,34 @@
 Split the string into an array, sort the array.
 return a template literal string with referencing the first and last index of the array in correct places.
 */
+
+const numbers = "4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"
+
+function highAndLow(numbers) {
+    console.log(numbers)
+    console.log(numbers.split(" ").sort((a, b) => a - b))
+    console.log(numbers)
+
+    const sortedNums = numbers.split(" ").sort((a, b) => a - b)
+    return `${sortedNums[sortedNums.length - 1]} ${sortedNums[0]}`
+}
+
+console.log("returns", highAndLow(numbers))
+
+//final
+function highAndLow(numbers) {
+    const sortedNums = numbers.split(" ").sort((a, b) => a - b)
+    return `${sortedNums[sortedNums.length - 1]} ${sortedNums[0]}`
+}
+
+//Math.max.apply example
+function highAndLow(numbers) {
+    numbers = numbers.split(' ').map(Number)
+    return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers)
+}
+
+//Min Max without apply
+function highAndLow(numbers) {
+    numbers = numbers.split(' ')
+    return `${Math.max(...numbers)} ${Math.min(...numbers)}`
+}
