5ec7dddfe8587e6a28216b572de34b629db406ed
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-26 21:24:37 -0600

    Add remove-1st-and-last-letter.js

---
+++ JavaScript/8kyu/remove-1st-and-last-letter.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
+
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+// Pseudo => return str slice from 1 to the final character in string
