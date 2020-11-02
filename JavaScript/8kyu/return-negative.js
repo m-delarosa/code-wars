7df0bbd2d2a88274fe4cfe2ea84909958f8583b9
github username <56710845+m-delarosa@users.noreply.github.com>
2020-11-01 17:53:44 -0700

    Finalize return-negative.js

--- JavaScript/8kyu/return-negative.js
+++ JavaScript/8kyu/return-negative.js
@@ -10,3 +10,5 @@
 
 // The number can be negative already, in which case no change is required.
 // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+const makeNegative = num => num < 0 ? num : num * -1
