7dacfabf26e67761d9d6e3769974e62263f6195e
github username <56710845+m-delarosa@users.noreply.github.com>
2020-11-02 23:55:26 -0700

    Add problem statement to mentis-challenge.js.

---
+++ JavaScript/Other/mentis-challenge.js
@@ -0,0 +1,12 @@
+/*
+For array A and B, return an array that contains values represented in both A and B.
+*/
+
+const nonDuplicateA = [1, 2, 3, 4, 5, 6]
+const nonDuplicateB = [2, 4, 6, 8]
+
+const findCommonValues = (a, b) => {
+
+}
+
+console.assert(findCommonValues([1, 2, 3, 4, 5, 6], [2, 4, 6, 8]))
