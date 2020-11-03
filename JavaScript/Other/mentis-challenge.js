2fc80f47df983da206c0ad84dfe432aa49e39f76
github username <56710845+m-delarosa@users.noreply.github.com>
2020-11-03 00:07:18 -0700

    Add first solution to mentis challenge.

--- JavaScript/Other/mentis-challenge.js
+++ JavaScript/Other/mentis-challenge.js
@@ -2,11 +2,27 @@
 For array A and B, return an array that contains values represented in both A and B.
 */
 
-const nonDuplicateA = [1, 2, 3, 4, 5, 6]
-const nonDuplicateB = [2, 4, 6, 8]
-
 const findCommonValues = (a, b) => {
+    let result = []
+    let shortArr, longArr
 
+    if (a.length >= b.length) {
+        shortArr = b
+        longArr = a
+    } else {
+        shortArr = a
+        longArr = b
+    }
+
+    result = shortArr.filter((num) => longArr.includes(num))
+
+    return result
 }
 
-console.assert(findCommonValues([1, 2, 3, 4, 5, 6], [2, 4, 6, 8]))
+//Tests for unique values within each array
+console.log(findCommonValues([1, 2, 3, 4, 5, 6], [2, 4, 6, 8]))
+console.assert(findCommonValues([1, 2, 3, 4, 5, 6], [2, 4, 6, 8]), [2, 4, 6])
+
+//Test for when redudant values occur in both arrays (2)
+console.log(findCommonValues([1, 2, 3, 2, 4, 5, 6], [2, 4, 2, 6, 8]))
+console.assert(findCommonValues([1, 2, 3, 2, 4, 5, 6], [2, 4, 2, 6, 8]), [2, 4, 6]) 
