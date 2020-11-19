70cbe0be2570c7d68c285e39c1b6af884cd854cd
github username <56710845+m-delarosa@users.noreply.github.com>
2020-11-18 18:26:51 -0700

    Finalize Herman's challenge

--- JavaScript/Other/hermans-challenge.js
+++ JavaScript/Other/hermans-challenge.js
@@ -4,7 +4,6 @@
    for me to create a solution that takes into accounts spaces and multiple words. */
 
 // Test Variables
-
 // Testing for word length discrpency edge case, should return false
 const edgeCaseTestStr1 = "longer"
 const edgeCaseTestStr2 = "short"
@@ -20,13 +19,20 @@ const negativeTestStr2 = "raceBar"
 /* Pseudo Code
     Handle Edge Case: First check to see if the length of each string is equal, if false, immediately return false
 
+    Otherwise, create an array out of the lowercase version of each character contained in input strings...
+    sort them to establish a comparable syntax between the two
+
+    Return the result of a conditional, which compares each array converted back to a string
+    ** We cannot simply compare the two arrays we created above, becuase it will fail to detect anagrams since...
+       arrays do not take into account syntax of list when using comparisoin operator. Therefore,  they must be...
+       compared as strings.
 */
 
 const checkAnagrams = (str1, str2) => {
     if (str1.length !== str2.length) {
         return false
     }
-    
+
     const arr1 = str1.toLowerCase().split("").sort()
     const arr2 = str2.toLowerCase().split("").sort()
 
@@ -35,5 +41,5 @@ const checkAnagrams = (str1, str2) => {
 
 //Tests
 console.log("Edgecase test, should return false:", checkAnagrams(edgeCaseTestStr1, edgeCaseTestStr2))
-console.log("Positive test, should return true:", checkAnagrams( positiveTestStr1, postiveTestStr2))
+console.log("Positive test, should return true:", checkAnagrams(positiveTestStr1, postiveTestStr2))
 console.log("Negative test, should return false:", checkAnagrams(negativeTestStr1, negativeTestStr2))
